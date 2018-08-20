8394d4c0b97a4cc067654d25d22a4dcbf73cf2ad
AmiK2001 <AmiK2001@spaces.ru>
2018-08-20 14:30:06 +0300

    Full bot refactoring without debug

--- bot.py
+++ bot.py
@@ -1,111 +1,130 @@
-import telebot
-import time
+''' Главный файл бота с его поведением '''
 import sqlite3
-import config #файл с токеном
-import text
-from telebot import types
 
-textrus = dict(reptop='Топ репутации', msgtop='Топ сообщений')
-texteng = dict(reptop='Top of reputation', msgtop='Top of message')
-textruseng = dict(botinchat='''🇷🇺 Привет, что бы испльзовать мои функции,
-вам сначало надо [настроить](t.me/animedev_bot?start=123) меня.
-🇺🇸 Hello, if you want to use a bot, 
-you should [set settings](t.me/animedev_bot?start=123) for me.''')
+import config  # Файл с токеном
+import telebot
 
-bot = telebot.TeleBot(config.token) #обьявляем токен
+bot = telebot.TeleBot(config.token) # Обьявляем токен
 
-class DataConn:
+class DBConnector:
+    """Класс для простого управления файлом БД"""
+
     def __init__(self, db_name):
         self.db_name = db_name
 
     def __enter__(self):
         self.conn = sqlite3.connect(self.db_name)
-        return self.conn
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
         if exc_val:
             raise
 
-@bot.message_handler(content_types = ['new_chat_members'])
-def message_handler(msg):
-    bot.send_message(msg.chat.id, textruseng.get("botinchat") , parse_mode= 'Markdown')
+db = DBConnector("username.db")
 
-@bot.message_handler(commands=['topmsg'])
-def handle_top_ms(msg):
-    def topbymsg(listnum):
-        conn = sqlite3.connect("username.db")  # или :memory: чтобы сохранить в RAM
-        cursor = conn.cursor()
-        sql = cursor.execute("SELECT name FROM users ORDER BY msgvalue DESC")
-        sortednames = sql.fetchall()[listnum * 10 - 10:10 * listnum]
-        newsort = []
-        for i in range(10):
-            try:
-                name = sortednames.pop()[0]
-            except IndexError:
-                return None
-            data = [name,
-                    cursor.execute("SELECT msgvalue FROM users WHERE name = ?", [(name)]).fetchall()[0][0]]
-            newsort.insert(0, data)
-        return newsort
-    l1 = topbymsg(2)
-    l = topbymsg(1)
-    if l1 is not None:
-        msgs = " "
-        for i in range(10):
-            msgs = msgs + "{username}: {karma}\n".format(username=l[i][0], karma=l[i][1])
-            print(msgs)
-        keyboard = types.InlineKeyboardMarkup()
-        callback_next = types.InlineKeyboardButton(text="▶", callback_data="next")
-        keyboard.add(callback_next)
-        bot.send_message(msg.chat.id, "страница 1\n" + msgs, reply_markup=keyboard)
-    if l1 is None:
-        msgs = " "
-        for i in range(10):
-            msgs = msgs + "{username}: {karma}\n".format(username=l[i][0], karma=l[i][1])
-            print(msgs)
+# Языковые словари
+class Language:
+    """Класс хранящий переводы"""
+    rus = dict(reptop='Топ репутации', msgtop='Топ сообщений')
+    eng = dict(reptop='Top of reputation', msgtop='Top of messages')
+    all = dict(botinchat="🇷🇺 Привет! Чтобы начать использовать бота,"+\
+    " его нужно [настроить](t.me/animedev_bot?start=123).\n"+\
+    "🇺🇸 Hello! To use bot, you should [setup](t.me/animedev_bot?start=123).")
 
+class Controller():
+    """ Тут методы для работы с данными """
+    def topbymsgform(self, page=1, PageIsChanged=False):
+        """" Формирует и отправляет топ по сообщениям """
+        l = topbymsg(page)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    def topbymsg(listnum):
-        conn = sqlite3.connect("username.db")  # или :memory: чтобы сохранить в RAM
-        cursor = conn.cursor()
-        sql = cursor.execute("SELECT name FROM users ORDER BY msgvalue DESC")
-        sortednames = sql.fetchall()[listnum * 10 - 10:10 * listnum]
-        newsort = []
-        for i in range(10):
-            try:
-                name = sortednames.pop()[0]
-            except IndexError:
-                return None
-            data = [name,
-                    cursor.execute("SELECT msgvalue FROM users WHERE name = ?", [(name)]).fetchall()[0][0]]
-            newsort.insert(0, data)
-        return newsort
-    keyboard = types.InlineKeyboardMarkup()
-    if call.message:
-        if call.data == "next":
-            l1 = 1
-            l = topbymsg(l1)
+        if l is not None:
+            rank = page*10-10
             msgs = " "
-            l1 = l1 + 1
-            for i in range(10):
-                if l1 is not None:
-                    msgs = " "
-                    for i in range(10):
-                        msgs = msgs + "{username}: {karma}\n".format(username=l[i][0], karma=l[i][1])
-                        print(msgs)
-                    keyboard = types.InlineKeyboardMarkup()
-                    callback_next = types.InlineKeyboardButton(text="▶", callback_data="next")
-                    keyboard.add(callback_next)
-                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                          text="Страница {}\n{}".format(str(l1), msgs), reply_markup=keyboard)
+            for i in len(l):
+                msgs = msgs + "{rank}.{username}: {karma}\n".format(username=l[i][0],\
+                karma=l[i][1], rank=rank)
+                rank = rank + 1
 
+            keyboard = telebot.types.InlineKeyboardMarkup()
 
+            if topbymsg(page+1) != None: # Проверяем, есть ли следующяя страница
+                callback_next = telebot.types.InlineKeyboardButton(text="→",\
+                callback_data=["next", page])
+                keyboard.add(callback_next)
 
+            if page != 1:
+                callback_back = telebot.types.InlineKeyboardButton(text="←",\
+                callback_data=["back", page])
+                keyboard.add(callback_back)
+
+            if page == 1 and not PageIsChanged:
+                bot.send_message(telebot.msg.chat.id, "Страница", page,\
+                "\n" + msgs, reply_markup=keyboard)
+
+            elif PageIsChanged:
+                return [msgs, page]
+
+        def topbymsg(listnum):
+            """ Возвращает список участников по номеру страницы """
+
+            with open(db) as database:
+                sql = database.execute("SELECT name FROM users ORDER BY msgvalue DESC")
+
+                sortednames = sql.fetchall()[listnum * 10 - 10:10 * listnum]
+                newsort = []
+
+                for i in range(10):
+                    try:
+                        name = sortednames.pop()[0]
+                    except IndexError:
+                        return None
+                    data = [name,
+                            database.execute("SELECT msgvalue FROM users WHERE name = ?",\
+                            [(name)]).fetchall()[0][0]]
+                    newsort.insert(0, data)
+                return newsort
+
+class Handlers:
+    """Класс со всеми хендлерами"""
+    def __init__(self):
+        @bot.message_handler(content_types=['new_chat_members'])
+        def welcome_handler(msg):
+            """" Приветствует новых участников """
+            bot.send_message(msg.chat.id, Language.all.get("botinchat"), parse_mode='Markdown')
+
+        @bot.message_handler(commands=['topmsg'])
+        def handle_top_msg():
+            """ Топ сообщений по количеству """
+            Controller.topbymsgform(1)
+
+        @bot.message_handler(commands=['warning'])
+        def handle_send_warning(msg):
+            """ ВНИМАНИЕ """
+            bot.send_message(msg.chat.id, "!!! ВНИМАНИЕ !!!\n"\
+            + "Спасибо за внимание.", parse_mode='Markdown')
+
+        @bot.callback_query_handler(func=lambda call: True)
+        def callback_inline(call):
+            """" Обработка обратного вызова """
+            keyboard = telebot.types.InlineKeyboardMarkup()
+
+            if call.message:
+                if "next" in call.data:
+                    msg = Controller.topbymsgform(call.data[1]+1, True)
+                    bot.edit_message_text(chat_id=call.message.chat.id,\
+                    message_id=call.message.message_id,\
+                    text="Страница {}\n{}".format(msg[1], msg[0]),\
+                    reply_markup=keyboard)
+                elif "back" in call.data:
+                    msg = Controller.topbymsgform(call.data[1]-1, True)
+                    bot.edit_message_text(chat_id=call.message.chat.id,\
+                    message_id=call.message.message_id,\
+                    text="Страница {}\n{}".format(msg[1], msg[0]),\
+                    reply_markup=keyboard)
 
 
 if __name__ == '__main__':
-   bot.polling(none_stop=True)
-
+    handlers = Handlers()
+    bot.polling(none_stop=True)
