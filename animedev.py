cedc9aef081b17f430efb7737705c5e901aa78a5
GrizzlyZeus <41058562+GrizzlyZeus@users.noreply.github.com>
2018-09-21 20:31:31 +0300

    Update animedev.py

--- animedev.py
+++ animedev.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import time
 import random
 import telebot
@@ -5,23 +6,12 @@ from telebot import types
 
 bot = telebot.TeleBot("634522342:AAHWAJrI3Mkp1_2KcHkwkQg7SwURtoA2nlA")
 
+print("Бот запущен\nСоедениние")
+bot.send_message(-1001160331786, "Бот запущен")
 
 @bot.message_handler(content_types=["new_chat_members"])
 def default_test(message):
-    bot.restrict_chat_member(message.chat.id, message.from_user.id, 1, False)
-    keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="Зайти в чат", callback_data="test")
-    keyboard.add(callback_button)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и прочитай [правила](https://t.me/animedev/101159).", reply_markup=keyboard, parse_mode='Markdown')
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    # Если сообщение из чата с ботом
-    if call.message:
-        if call.data == "test":
-            bot.restrict_chat_member(call.message.chat.id, call.from_user.id, can_send_messages = True)
-
+    bot.send_message(message.chat.id, "Привет! Обязательно прочитай [правила](https://t.me/animedev/101159).", parse_mode='Markdown')
 
 @bot.message_handler(commands=["banme"])
 def default_tesdt(message):
@@ -36,9 +26,45 @@ def default_tesdt(message):
     else:
         bot.reply_to(message,
                          "[{0}](tg://user?id={1}) ".format(message.from_user.first_name, message.from_user.id)
-                         + "Извините, вы администратор чата.",
+                         + "Извините, вы администратор чата.\nИли уже забанены.",
                          parse_mode='Markdown')
 
+@bot.message_handler(commands = ["mute"])
+def mute(msg):
+    try:
+        if bot.get_chat_member(msg.chat.id, msg.from_user.id).status != "member":
+            if bot.get_chat_member(msg.chat.id, msg.reply_to_message.from_user.id).status == "member":
+                if msg.reply_to_message is not None:
+                    if 1 < len(msg.text.split(" ")) < 3:
+                        bantime = int(" ".join(msg.text.split(' ')[1:]))
+                        if bantime > 30 and bantime < 9999999:
+                            bot.send_message(msg.chat.id, "[{0}](tg://user?id={1}) "
+                                             .format(msg.reply_to_message.from_user.first_name,
+                                                     msg.reply_to_message.from_user.id, ) +
+                                             "Забанен на " + str(bantime) + " сек ",
+                                             parse_mode='Markdown')
+                            bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id,
+                                                     int(time.time() + bantime),
+                                                     can_send_messages=False)
+                        else:
+                            bot.send_message(msg.chat.id,
+                                             "Число " + str(
+                                                 bantime) + " слишком большое или маленькое.\nДиапозон 30-9999999")
+                    else:
+                        bot.send_message(msg.chat.id, "Некоректное время")
+                else:
+                    bot.send_message(msg.chat.id, "Используй только реплаем!")
+            else:
+                bot.send_message(msg.chat.id, "Пользователь является администратором")
+        else:
+            bot.send_message(msg.chat.id, "Вы не администратор чата.")
+    except ValueError:
+        bot.send_message(msg.chat.id, "Некоректное время бана")
+
+
+@bot.message_handler(commands = ["stop"])
+def stop(msg):
+    bot.send_message(-1001160331786, "Бот отключен")
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.polling(none_stop=True, timeout=120)
